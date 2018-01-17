6906119884e0965b833fbd36d8abf3b7436129cc
Indrė <krasauskaite.i@gmail.com>
2018-01-17 21:32:17 +0200

    Intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule, MatCardModule } from '@angular/material';
 import 'hammerjs';
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { IntroComponent } from './intro/intro.component';
 
 import { RandomizeWordsService } from './randomize-words.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
