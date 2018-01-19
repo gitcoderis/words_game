6d4a6f97eec178c7e78e1b3cd809472a259cfd14
Elenazuokaite <elena.zuokaite@gmail.com>
2018-01-19 19:44:04 +0200

    New function in game ts

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,25 +1,24 @@
 // Elena: irasiau jquery, sukuriau custom.js
 // Bandau sukurti funkcija, kuri atvaizduotu tuscia kryziazodi
-$(document).ready(function () {
-    makeGrid(5);
-    $(".letter").click(function () {
-        let letter = $("td").text();
-        letter = $(this).html();
-        $("#0").text($(this).html());
-        console.log($(this).html());
-        console.log($("td").text());
-    })
-});
-function makeGrid(x) {
-    if ($('.test')) {
-        $('.test').remove();
-    };
-    for (var rows = 0; rows < x; rows++) {
-        $("<div class='test' id='"+rows+"'></div>").appendTo("#crossword-grid");
-    };
-    $(".test").width(400 / x);
-    $(".test").height(40);
-    $(".test").css("border", "1px solid #000");
-    $(".test").css("display", "inline-block");
-};
-
+// $(document).ready(function () {
+//     makeGrid(5);
+//     $(".letter").click(function () {
+//         let letter = $("td").text();
+//         letter = $(this).html();
+//         $("#0").text($(this).html());
+//         console.log($(this).html());
+//         console.log($("td").text());
+//     })
+// });
+// function makeGrid(x) {
+//     if ($('.test')) {
+//         $('.test').remove();
+//     };
+//     for (var rows = 0; rows < x; rows++) {
+//         $("<div class='test' id='"+rows+"'></div>").appendTo("#crossword-grid");
+//     };
+//     $(".test").width(400 / x);
+//     $(".test").height(40);
+//     $(".test").css("border", "1px solid #000");
+//     $(".test").css("display", "inline-block");
+// };
